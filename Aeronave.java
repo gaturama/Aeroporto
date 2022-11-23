9e7a3d6512ce59ced1792d4f2933aa495e04cb14
gaturama <bielsobv@gmail.com>
2022-11-23 20:46:43 -0300

    Primeiras classes do projeto adicionadas

---
+++ Aeronave.java
@@ -0,0 +1,10 @@
+public class Aeronave {
+    
+    private String modelo;
+    private String companhia;
+
+    public Aeronave(String modelo, String companhia) {
+        this.modelo = modelo;
+        this.companhia = companhia;
+    }
+}
