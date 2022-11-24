82b20b0aec89a402b0ea30c3ea793a5b48579d2e
gaturama <bielsobv@gmail.com>
2022-11-23 22:15:09 -0300

    adição de comandos

--- Aeronave.java
+++ Aeronave.java
@@ -1,10 +1,32 @@
-public class Aeronave {
+public abstract class Aeronave {
     
     private String modelo;
     private String companhia;
 
-    public Aeronave(String modelo, String companhia) {
+    protected Aeronave(
+     String modelo,
+     String companhia
+    ) {
         this.modelo = modelo;
         this.companhia = companhia;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public String getCompanhia() {
+        return companhia;
+    }
+    public void setCompanhia(String companhia) {
+        this.companhia = companhia;
+    }
+
+    @Override
+    public String toString() {
+        return "Modelo: " + modelo + "\n" +
+               "Companhia: " + companhia + "\n";
     }
 }
